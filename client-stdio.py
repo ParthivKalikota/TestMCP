282bf461afc3c809bec966a026ae2f645f645404
ParthivKalikota <parthivkalikota@gmail.com>
2025-06-09 17:41:17 +0530

    Added LLM to create a query

--- client-stdio.py
+++ client-stdio.py
@@ -2,6 +2,8 @@ import asyncio
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 import json
+import os
+from openai import OpenAI
 
 async def main():
     server_params = StdioServerParameters(
@@ -21,7 +23,34 @@ async def main():
 
             print("\nClient: Calling query_db tool...")
             try:
-                result = await session.call_tool("query_db", arguments={'query' : 'select product_name from sales'})
+                user_query = input("Enter Your Prompt : ")
+                client = OpenAI(
+                    # This is the default and can be omitted
+                    api_key=os.environ.get("OPENAI_API_KEY"),
+                )
+
+                response = client.responses.create(
+                    model="gpt-4o",
+                    instructions="""You are an expert in RDBMS and you are a professional in querying these SQL Databases. I want you to fetch the data from a postgreSQL database. I will provide the table Name and the schema of the table. Your Tassk is to write an accurate and a valid query. The Output should be a valid string.
+                    Here are the details of the table
+                    Table Name : sales
+                    Schema of sales Table: 
+                    sale_id : integer
+                    product_name : string
+                    category : string
+                    region : string
+                    quantity : integer
+                    unit_price : numeric(10,2)
+                    total_sale : numeric(10,2)
+                    sales_rep : string
+                    
+                    """,
+                    input=user_query,
+                )
+
+                query = response.output_text
+                print(query)
+                result = await session.call_tool("query_db", arguments={'query' : query[6:-3]})
                 print(result)
                 if result.content and result.content[0].text:
                     # The content is a list of parts, usually one text part
